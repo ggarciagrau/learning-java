4e4bb7db01b0484f4b954969f61897deb7e60d8e
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-01-14 23:30:58 +0100

    Variables - strings and integers

---
+++ src/_0_variablesinjava/TypeInference.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _0_variablesinjava;
+
+/**
+ *
+ * @author gerardgg
+ */
+public class TypeInference {
+    
+    public static void run() {
+        var age = 20;
+        var name = "Roberto";
+        
+        System.out.println("Hello " + name + ", you're " + age + " years old");
+        
+        // soutv
+        System.out.println("name = " + name);
+    }
+    
+}
---
+++ src/_0_variablesinjava/TypeIntegerAndTypeString.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _0_variablesinjava;
+
+/**
+ *
+ * @author gerardgg
+ */
+public class TypeIntegerAndTypeString {
+
+    public static void run() {
+        int integer = 0;
+        System.out.println("My integer is " + integer);
+
+        String name = "Roberto";
+        System.out.println("Hello " + name);
+    }
+}
